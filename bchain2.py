b5087876c04c2b3e92ad76221ee927b4b30e6daa
Arpan Halder <tech.halder.arpan@gmail.com>
2022-03-21 21:42:39 +0530

    Update bchain2.py

--- bchain2.py
+++ bchain2.py
@@ -8,12 +8,6 @@ Created on Mon Mar 21 20:30:50 2022
 
 import hashlib
 
-
-def hashGenerator(data):
-    result = hashlib.sha256(data.encode())
-    return result.hexdigest()
-
-
 class Block:
     def __init__(self, data,index, prev_index, prev_hash): #hash we create after class is constructed
         self.data = data
@@ -46,4 +40,15 @@ t2 = Block("A > B 100",2,1,t1.hash())
 t3 = Block("Bank > C 2000",3,100,Gen.hash())
 t4 = Block("B > C 50",4,2,t2.hash())
 t5 = Block("C > A 200",5,3,t3.hash())
+
+
+print( t4.prev_hash)
+
+print(t2.hash())
+print(t2.prev_hash)
+
+print(t1.hash())
+print(t1.prev_hash)
+
+print(Gen.hash())
 
